81ffa4f9eb3e007a859003c3dbe7832ff0688a78
Paritosh Mehta <mehta.paritosh95@gmail.com>
2024-12-04 16:35:13 +0530

    init

--- README.md
+++ README.md
@@ -1,17 +1,128 @@
-## My Project
+## AWS IoT SiteWise Stream Publisher
 
-TODO: Fill this README out!
+This component monitors the local MQTT topic `local/swe_stream/publish` and pushes the incoming data to the `SiteWise_Edge_Stream` stream. 
 
-Be sure to:
+**Note:** To enable features offered by  **AWS IoT SiteWise Edge**, please enable the **Data Processing Pack** when configureing the **AWS IoT Greengrass Core** as an **AWS IoT SiteWise Edge Gateway**. 
 
-* Change the title in this README
-* Edit your repository description on GitHub
+The incoming message to the topic must be of the following format: 
 
-## Security
+```
+{
+    "timestamp": "1680869520.993",
+    "alias": "Gnn6000_21-101-AGICODE",
+    "value": 4.0,
+    "quality": "GOOD"
+}
+```
+This component takes away the heavy lifting of creating the sitewise streams and adding data to it. 
+
+You can also change the name of the stream from `SiteWise_Edge_Stream` to `SiteWise_Stream` if you want to send data directly to the cloud. 
+
+![Architecture Diagmram](architecture.png) 
+
+## Downloading this component
+You can either clone this repo with git, or download this component as a zip file.  
+
+NOTE THAT THE FOLDER IN WHICH YOU CLONE OR UNZIP THIS COMPONENT NEEDS TO BE CALLED ```aws-greengrass-labs-sitewise-stream-publisher``` FOR GDK TO WORK PROPERLY
+
+## Installing dependencies
+It is recommended to create a virtual environment to install dependencies. To do so, run the following commands in the root folder of this repo:  
+```bash
+ python3 -m venv .venv
+ source ./.venv/bin/activate
+```
+
+This component can be built with [GDK](https://docs.aws.amazon.com/greengrass/v2/developerguide/gdk-cli-configuration-file.html)
+
+```bash
+python3 -m pip install -U git+https://github.com/aws-greengrass/aws-greengrass-gdk-cli.git@v1.2.3
+```
+## Build
+
+Before building the component, you will need to update the gdk-config.json file, replacing the bucket and region placeholder.
+```     .
+        .
+        "publish": {
+            "bucket": "<PLACEHOLDER BUCKET HERE>",
+            "region": "<PLACEHOLDER REGION>"
+        }
+        .
+        .
+```
+The bucket is where the component artefact will be uploaded when you publish the component, and region is the region where the greengrass component will be created.
+
+Once this is done, you can build the component with the following command:
+```
+gdk component build
+```
+## Publish
+Before you can deploy **aws.greengrass.labs.sitewise.stream.publisher** component to your device, you first need to publish your component.
+This can be done with the following command:
+```
+gdk component publish
+```
+
+You should now be able to see the **aws.greengrass.labs.sitewise.stream.publisher** component in the *Greengrass->component* section of the AWS console.
+
+It can now be included in a Greengrass deployment and pushed to your device.
+
+## Configuration
+
+This component provides the following configuration parameters when it is deployed:
+
+```
+StreamName: "SiteWise_Edge_Stream"
+topicName: "local/swe_stream/publish"
+```
+
+You can chose any value for **topicName** depending on your application, however, valid examples for **StreamName** are:
+```
+SiteWise_Edge_Stream
+SiteWise_Stream
+``` 
+
+Further, please make sure that the  **GreengrassV2TokenExchangeRole** IAM Role has access to AWS IoT Sitewise as mentioned in the Requirements documentation for [AWS IoT SiteWise with Edge Processing](https://docs.aws.amazon.com/iot-sitewise/latest/userguide/edge-setup.html)
+
+## Local log file
+This component uses the following local log file:
+```
+/greengrass/v2/logs/aws.greengrass.labs.sitewise.stream.publisher.log
+```
+
+## Incoming Topic Payload Considerations
+
+Expected paylaod structure is the following:
+
+```
+{
+    "timestamp": "1680869520.993",
+    "alias": "Gnn6000_21-101-AGICODE",
+    "value": 4.0,
+    "quality": "GOOD"
+}
+```
+
+1. Please note that the value of the key: **"timestamp"** should be a **STRING**, and should follow the epoch seconds format. 
+    Example python code: 
+
+    import time
+    timestamp= str(time.time())
+
+2. Value of the key: **"value"** should be a **FLOAT**.
+    Example python code: 
+
+    value= float(4)
+    
+3. Value of the key: **"alias"** should be a **STRING**, and this alias should be associated with an **ASSET** in **AWS IoT SiteWise**.
+4. Value of the key: **"quality"** should be a **STRING**, and one of 3 possible values: **GOOD | BAD | UNCERTAIN**
+    Example python code: 
+    
+    quality= "UNCERTAIN"
+
+## Contributing and Security
 
 See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
 
 ## License
 
-This project is licensed under the Apache-2.0 License.
-
+This library is licensed under the Apache-2.0 License. See the LICENSE file.
---
+++ main.py
@@ -0,0 +1,186 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+'''
+SAMPLE TOPIC PAYLOAD:
+    {
+        "timestamp": "1680869520.993",
+        "alias": "Gnn6000_21-101-AGICODE",
+        "value": 4.0,
+        "quality": "GOOD"
+    }
+
+Code Considerations:
+    1. Note that the value of timestamp should be a STRING, and should follow the epoch seconds format. 
+       Example python code: 
+    
+        import time
+        timestamp= str(time.time())
+    
+    2. Value of the key: "value" should be a FLOAT.
+       Example python code: 
+    
+        value= float(4)
+        
+    3. Value of the key: "alias" should be a STRING, and this alias should be associated with an ASSET in AWS IoT SiteWise.
+    4. Value of the key: "quality" should be a STRING, and one of 3 possible values: GOOD | BAD | UNCERTAIN
+       Example python code: 
+       
+        quality= "UNCERTAIN"
+    
+'''
+
+
+import sys
+import time
+import traceback
+import json
+
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
+from awsiot.greengrasscoreipc.model import (
+    SubscriptionResponseMessage,
+    UnauthorizedError
+)
+
+# Stream Manager imports
+import asyncio
+import calendar
+import logging
+import random
+import time
+import uuid
+from stream_manager import (
+    AssetPropertyValue,
+    ExportDefinition,
+    IoTSiteWiseConfig,
+    MessageStreamDefinition,
+    PutAssetPropertyValueEntry,
+    Quality,
+    ResourceNotFoundException,
+    StrategyOnFull,
+    StreamManagerClient,
+    TimeInNanos,
+    Variant,
+)
+from stream_manager.util import Util
+import unicodedata
+
+# Logging config
+logging.basicConfig(level=logging.INFO)
+
+# Convert string to a caseless format for comparison
+def normalize_caseless(text):
+    return unicodedata.normalize("NFKD", text.casefold())
+
+# Function to publish payload to stream
+def swe_stream_pub(alias, timestamp, value, quality="GOOD"):
+    
+    # Define time
+    time_in_nanos= TimeInNanos(time_in_seconds=timestamp)
+    # Defing value
+    variant= Variant(double_value=value)
+    # Define quality
+    if normalize_caseless(quality)=="good":
+        quality=Quality.GOOD
+    elif normalize_caseless(quality)=="bad":
+        quality=Quality.BAD
+    elif normalize_caseless(quality)=="uncertain":
+        quality=Quality.UNCERTAIN
+        
+    # Define asset entry
+    asset = [AssetPropertyValue(value=variant, quality=quality, timestamp=time_in_nanos)]
+    entry= PutAssetPropertyValueEntry(entry_id=str(uuid.uuid4()), property_alias=alias, property_values=asset)
+    # Convert to JSON Bytes
+    json_bytes= Util.validate_and_serialize_to_json_bytes(entry)
+    # Append to stream
+    sequence_number= sm_client.append_message(stream_name, json_bytes)
+    
+    # Confirm data entry success
+    print('Appended message to stream_name: %s sequence_number: %s message: %s', stream_name, sequence_number, json_bytes.decode("utf-8"))
+    
+# Main function
+def main(logger):
+    args = sys.argv[1:]
+    topic = args[0]
+    global stream_name
+    stream_name = args[1]
+
+    try:
+        
+        ## Stream manager stream and client setup
+        global sm_client
+        sm_client = StreamManagerClient()
+        # Try deleting the stream (if it exists) so that we have a fresh start
+        try:
+            sm_client.delete_message_stream(stream_name=stream_name)
+        except ResourceNotFoundException:
+            pass
+
+        exports = ExportDefinition(
+            iot_sitewise=[IoTSiteWiseConfig(identifier="IoTSiteWiseExport" + stream_name, batch_size=5)]
+        )
+        sm_client.create_message_stream(
+            MessageStreamDefinition(
+                name=stream_name, strategy_on_full=StrategyOnFull.OverwriteOldestData, export_definition=exports
+            )
+        )
+        
+        ## Subscribe to local topic
+        global ipc_client
+        ipc_client = GreengrassCoreIPCClientV2()
+        # Subscription operations return a tuple with the response and the operation.
+        _, operation = ipc_client.subscribe_to_topic(topic=topic, on_stream_event=on_stream_event,
+                                                     on_stream_error=on_stream_error, on_stream_closed=on_stream_closed)
+        print('Successfully subscribed to topic: ' + topic)
+
+        
+        # Keep the main thread alive, or the process will exit.
+        try:
+            while True:
+                time.sleep(10)
+        except InterruptedError:
+            print('Subscribe interrupted.')
+
+        # To stop subscribing, close the stream.
+        operation.close()
+    except UnauthorizedError:
+        print('Unauthorized error while subscribing to topic: ' +
+              topic, file=sys.stderr)
+        traceback.print_exc()
+        exit(1)
+    except Exception:
+        print('Exception occurred', file=sys.stderr)
+        traceback.print_exc()
+        exit(1)
+
+def on_stream_event(event: SubscriptionResponseMessage) -> None:
+    try:
+        message = str(event.binary_message.message, 'utf-8')
+        topic = event.binary_message.context.topic
+        print('Received new message on topic %s: %s' % (topic, message))
+        
+        # Fetch required values from message payload
+        message_json = json.loads(message)
+        alias= message_json['alias']
+        timestamp= int(float(message_json['timestamp']))
+        value= float(message_json['value'])
+        quality= message_json['quality']
+        
+        # Append message to Stream
+        swe_stream_pub(alias, timestamp, value, quality)
+    except:
+        traceback.print_exc()
+
+
+def on_stream_error(error: Exception) -> bool:
+    print('Received a stream error.', file=sys.stderr)
+    traceback.print_exc()
+    return False  # Return True to close stream, False to keep stream open.
+
+
+def on_stream_closed() -> None:
+    print('Subscribe to topic stream closed.')
+
+
+if __name__ == "__main__":
+   main(logger=logging.getLogger())
